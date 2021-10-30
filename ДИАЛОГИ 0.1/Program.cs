ee89b32f9f733639f7418ce4e8e38f0ca34c4bbd
Анна Валитова <anavalitova0@gmail.com>
2021-10-30 17:02:20 +1000

    Update

--- ДИАЛОГИ 0.1/Program.cs
+++ ДИАЛОГИ 0.1/Program.cs
@@ -20,48 +20,36 @@ namespace ДИАЛОГИ_0._1
 
                 stroka = reader.ReadInt32();// считываем кол-во строк
                 sdvig = reader.ReadInt32();// считываем сдвиг, начало 3-ей части
-                Console.WriteLine($"{stroka},{ sdvig}");//вывод на консоль
+                Console.WriteLine($"Количество строк:{stroka}, Cдвиг на часть 3: { sdvig} байт");//вывод на консоль
                 Console.ReadKey();//ожидание нажатия клавиши
 
-                int re = 21; //первый байт второй части файла
-                int zd = 0; // накопление сдвига
-                
                 int m = 0;
                 int zap1 = 0, zap2 = 0;
                 while (m < stroka)//цикл для чтения описания строк
                 {
-                    reader.BaseStream.Seek(28, SeekOrigin.Current);//пропуск 28 байт
-               // мы можем расчитать позицию записи относительно начала файла как m*40+21?? 
+                    reader.BaseStream.Seek(28, SeekOrigin.Current); //пропуск 28 байт
+                   
+                    zap1 = reader.ReadInt32(); //читаем сдвиг
+                    zap2 = reader.ReadInt32(); //читаем длину строки
+                                              
 
-                    Console.WriteLine(m);
-
-                    zap1 = reader.ReadInt32();//читаем сдвиг
-                    zap2 = reader.ReadInt32();//читаем длину строки; на втором проходе цикла
-                                              //!!строка не определяется!!
-                    zd = zd + zap1; //накапливаем сдвиг
+                    reader.BaseStream.Seek(4, SeekOrigin.Current); //пропуск 4 байт
+                    long pos = reader.BaseStream.Position; // запоминаем позицию во второй части файла
                     
 
-                    reader.BaseStream.Seek(4, SeekOrigin.Current);//пропуск 4 байт
-                    // можем ли мы каким-то  образом сохранить текущую позицию в переменную??
-                    re = re + 40;// следующие описание строки
-                  
-                        Console.WriteLine($"длина строки - {zap2} ");
-
-                    reader.BaseStream.Seek(zd + sdvig, SeekOrigin.Begin);//находим позицию начала текстовой
+                    reader.BaseStream.Seek(zap1 + sdvig, SeekOrigin.Begin); //находим позицию начала текстовой
                     //строки относительно начала файла
 
-                    byte[] bytes = reader.ReadBytes(zap2);//читаем строку определённой длины
+                    byte[] bytes = reader.ReadBytes(zap2); //читаем строку определённой длины
 
-                    Encoding u = Encoding.GetEncoding(0x4e3);
+                    Encoding u = Encoding.GetEncoding(0x4e3); // перекодировка из Юникода
                     string text = u.GetString(bytes);
 
-                    Console.WriteLine($"{m}; {re}; {zap1}; {zap2}; ");
-                    Console.WriteLine($"%{text}%");
+                    //Console.WriteLine($"{m}; {pos}; {(m+1)*40+20}; {zap1}; {zap2}; ");
+                    Console.WriteLine($"Cтрока номер {m}: '{text}'");
 
-                   reader.BaseStream.Seek(re , SeekOrigin.Begin);//возвращаемся на следующую позицию описания
+                   reader.BaseStream.Seek(pos , SeekOrigin.Begin);//возвращаемся на следующую позицию описания
                                                                  //строк во второй части файла
-             // мы можем расчитать следующую позицию записи во второй части как (m+1)*40+21?? 
-
                     Console.ReadKey();
                     m++;
                 }
